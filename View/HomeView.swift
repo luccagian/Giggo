36f47890a59296459fdeb962b219bc8a35256ce7
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2026-05-27 01:01:03 -0300

    Update HomeView.swift

--- View/HomeView.swift
+++ View/HomeView.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct HomeView: View {
+    @EnvironmentObject var sessaoUser = UserSection
+
+    var user: User {
+        sessaoUser.atualUser
+    }
+    
     var body: some View {
         NavigationStack{
             ScrollView {
                 Rectangle()
                     .frame(width: 100, height: 5600)
             }
-            .navigationTitle("Olá, xxx!")
+            .navigationTitle("Olá, \(usuario.nome)!")
             .toolbar(content: {
                 Button(action: {
                     
