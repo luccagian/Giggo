e4183c154aaaa831be6c41f3f80fdbb511067604
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2026-05-27 00:53:41 -0300

    Update UserSection.swift

--- ViewModel/UserSection.swift
+++ ViewModel/UserSection.swift
@@ -1,1 +1,12 @@
+import Foundation
 
+class UserSection: ObservableObject {
+  @Published var atualUser: User = userArtista 
+
+  func trocaArtista() {
+    atualUser = userArtista
+  }
+
+  func trocaBar(){
+    atualUser = userBar
+  }
