d764c49798d6ad254c0c7e53fbeaa2e6eba694db
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2026-05-27 02:42:42 -0300

    Update HomeView.swift

--- View/HomeView.swift
+++ View/HomeView.swift
@@ -7,23 +7,53 @@ struct HomeView: View {
         sessaoUser.atualUser
     }
     
-    var body: some View {
-        NavigationStack{
-            ScrollView {
-                Rectangle()
-                    .frame(width: 100, height: 5600)
-            }
-            .navigationTitle("Olá, \(usuario.nome)!")
-            .toolbar(content: {
-                Button(action: {
-                    
-                }) {
-                    Image(systemName: "bell")
-                }
-            })
+    
+    var itensDaHome: [InfosBasicas] {
+        switch user.tipo {
+        case .artista:
+            return baresMock
             
-            
+        case .bar:
+            return artistasMock
         }
+    }
+    
+    var tituloRecomendados: String {
+        user.tipo == .artista ? "Casas em destaque" : "Artistas em destaque"
+    }
+    
+    var tituloNormais: String {
+        usuario.tipo == .artista ? "Outras casas disponíveis" : "Outros artistas disponíveis"
+    }
+    
+    var recomendados: [InfosBasicas] {
+        Array(
+            itensDaHome
+                .sorted { $0.avalia > $1.avalia }
+                .prefix(5)
+        )
+    }
+    
+    var cardsNormais: [InfosBasicas] {
+        let idsRecomendados = Set(recomendados.map { $0.id })
+        
+        return Array(
+            itensDaHome
+                .filter { !idsRecomendados.contains($0.id) }
+                .prefix(10)
+        )
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 28) {
+                headerSection
+                recomendadosSection
+                cardsNormaisSection
+            }
+            .padding(.vertical)
+        }
+        .background(Color(.systemGroupedBackground))
     }
 }
 
