823d715cbe67984d7e422632e30965d6bfbdc9d1
MarianaMayumeCaniza <76888332+MarianaMayumeCaniza@users.noreply.github.com>
2026-05-27 14:40:50 -0300

    Implement CalendarView with DatePicker and Navigation

--- View/CalendarView.swift
+++ View/CalendarView.swift
@@ -1,15 +1,77 @@
+//
+//  CalendarView.swift
+//  Projeto
+//
+//  Created by Aluno Mack on 27/05/26.
+//
+
 import SwiftUI
 
+
+//Definir o Background depois
+let backgroundDark = Color.blue
+
+/*LinearGradient(
+    colors: [Color.red, Color.blue],
+    startPoint: .top, endPoint: .bottom)*/
+
+
+
+/**
+ 
+ 
+ ------------   CALENDARIO     ----------------
+ 
+ **/
+
 struct CalendarView: View {
+    @State private var selectedDate = Date.now
+    @State private var path = NavigationPath()
+    
     var body: some View {
-        VStack{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            Rectangle()
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
-        }
         
-    }
-}
+        NavigationStack(path: $path){
+            
+            VStack(alignment: .center){
+                DatePicker(
+                    "Calendar",
+                    selection: $selectedDate,
+                    displayedComponents: .date
+                ).datePickerStyle(.graphical)
+                
+            }.frame(maxWidth: .infinity,
+                    maxHeight: .infinity,
+                    alignment: .top)//Stack da DatePicker
+            
+            .navigationDestination(for: Date.self ){
+                date in VStack{
+                    Text(date.description)
+                } //Destino da navegacao do calendario
+            }//NavigationDestination
+            
+            HStack(spacing: 40) {
+            }
+            
+            //Vai ter um filtro aqui talvez... se der tempo
+            ScrollView {
+                    VStack(alignment: .leading) {
+                        ForEach(0..<20) {
+                            Text("Row \($0)")
+                        }
+                    }
+                }
+            
+            
+        } //NavigationStack
+        .onChange(of: selectedDate) { _, newValue in
+                    path.append(newValue)}//onChange
+            
+        
+        
+        
+        
+    }//Body
+}//View
 
 #Preview {
     CalendarView()
