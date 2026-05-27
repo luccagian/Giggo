29df1fd4614c7894d276aa422423d1e3518c146b
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2026-05-27 00:36:32 -0300

    Create UserMock.swift

---
+++ Mocks/UserMock.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+let usuarioLogadoMock = Usuario(
+    nome: "Banda Aurora",
+    email: "banda.aurora@email.com",
+    tipo: .artista
+)
+
+let usuarioLogadoMock = Usuario(
+    nome: "Bar Central",
+    email: "contato@barcentral.com",
+    tipo: .bar
+)
